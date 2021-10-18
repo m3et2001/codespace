7d7ef115f5c9ae70b91ccdd106c89f767875bcb7
m3et2001 <84465263+m3et2001@users.noreply.github.com>
2021-10-18 22:28:46 +0530

    Update asgi.py

--- FTC_project/asgi.py
+++ FTC_project/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django 
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,7 +17,7 @@ from django.urls import path
 from FTC_users.consumers import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FTC_project.settings')
-django.setup()
+
 # application = get_asgi_application()
 
 ws_patterns=[
