a7f3ace9ce1e8b9260c0c39ac63088551a8d3aaf
m3et2001 <84465263+m3et2001@users.noreply.github.com>
2021-10-18 20:45:56 +0530

    Update settings.py

--- FTC_project/settings.py
+++ FTC_project/settings.py
@@ -99,6 +99,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'] = dj_database_url.config(default='postgres://...'}
+DATABASES['default'].update(db_from_env)
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
