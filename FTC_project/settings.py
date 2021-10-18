ce7b630d8c7033b0ddf021ad505cd566264355af
m3et2001 <84465263+m3et2001@users.noreply.github.com>
2021-10-18 21:07:59 +0530

    Update settings.py

--- FTC_project/settings.py
+++ FTC_project/settings.py
@@ -100,10 +100,10 @@ DATABASES = {
 }
 
 import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES['default'] = dj_database_url.config(default='postgres://...'}
-# DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 CHANNEL_LAYERS = {
     "default": {
